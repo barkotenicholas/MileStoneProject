19e3bcd842b0215d5aa9be3d8e623bc39e1e2a37
barkotenicholas <barkotenicholas@gmail.com>
2022-04-21 22:50:02 +0300

    Added report page

--- app/src/main/java/com/example/gbv/Home.java
+++ app/src/main/java/com/example/gbv/Home.java
@@ -2,13 +2,28 @@ package com.example.gbv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.gbv.databinding.ActivityHomeBinding;
 
 public class Home extends AppCompatActivity {
 
+    ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+
+
+        binding.button.setOnClickListener(view -> {
+                    startActivity(new Intent(this,Report.class));
+        });
     }
 }
--- app/src/main/java/com/example/gbv/Report.java
+++ app/src/main/java/com/example/gbv/Report.java
@@ -2,13 +2,69 @@ package com.example.gbv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import com.example.gbv.databinding.ActivityReportBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class Report extends AppCompatActivity {
 
+    ActivityReportBinding binding;
+    final Calendar myCalendar = Calendar.getInstance();
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_report);
+
+        binding = ActivityReportBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+
+        DatePickerDialog.OnDateSetListener date = (view, year, month, day) -> {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, month);
+            myCalendar.set(Calendar.DAY_OF_MONTH, day);
+             updateLabel();
+        };
+
+        binding.date.setEndIconOnClickListener(view -> new DatePickerDialog(Report.this,date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show());
+
+        binding.report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                collectData();
+            }
+        });
+
+    }
+
+    private void collectData() {
+
+        String name = binding.username.getText().toString();
+        String idno = binding.idnumber.getText().toString();
+        String date = binding.pickdate.getText().toString();
+        String location = binding.local.getText().toString();
+        String desc = binding.descr.getText().toString();
+
+        Log.d("TAG", "collectData: "+name+" "+idno+""+date+""+location+""+desc);
+    }
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
+        binding.pickdate.setText(dateFormat.format(myCalendar.getTime()));
     }
 }
