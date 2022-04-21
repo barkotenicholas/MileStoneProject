43230dc0f1237f9883d8d731fd7771de0c5661cf
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-04-21 07:50:49 +0300

    Edit Splash Animation

--- app/src/main/java/com/example/gbv/SplashActivity.java
+++ app/src/main/java/com/example/gbv/SplashActivity.java
@@ -2,13 +2,43 @@ package com.example.gbv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.gbv.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
+
+    TextView appname;
+    LottieAnimationView lottie;
+    //Binding Class
+    private ActivitySplashBinding activitySplashBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        //Get Layout
+        activitySplashBinding = ActivitySplashBinding.inflate(getLayoutInflater());
+        View view = activitySplashBinding.getRoot();
+        setContentView(view);
+        //Splash screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        activitySplashBinding.appname.animate().translationY(2000).setDuration(1000).setStartDelay(5000);
+        activitySplashBinding.lottie.animate().translationY(1500).setDuration(1000).setStartDelay(5000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+
+            }
+        }, 6000);
     }
 }
