b6946ff3ec492933df9907f617b989aff64b44df
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-04-22 01:58:34 +0300

    Add google sign in

--- app/src/main/java/com/example/gbv/LoginActivity.java
+++ app/src/main/java/com/example/gbv/LoginActivity.java
@@ -110,7 +110,7 @@ public class LoginActivity extends AppCompatActivity {
     private void createRequest() {
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(getString(R.string.default_web_client_id1))
                 .requestEmail()
                 .build();
 
