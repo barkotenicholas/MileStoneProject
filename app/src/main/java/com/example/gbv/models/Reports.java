7247c0900519f1c72f792cb3f953293b3d7974b2
barkotenicholas <barkotenicholas@gmail.com>
2022-04-21 22:57:28 +0300

    Added report pojo

---
+++ app/src/main/java/com/example/gbv/models/Reports.java
@@ -0,0 +1,61 @@
+package com.example.gbv.models;
+
+public class Report {
+
+    String name;
+    String id;
+    String date;
+    String location;
+    String desc;
+
+    public Report() {
+    }
+
+    public Report(String name, String id, String date, String location, String desc) {
+        this.name = name;
+        this.id = id;
+        this.date = date;
+        this.location = location;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
